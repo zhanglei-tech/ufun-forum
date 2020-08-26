be3940fdb00b8f3644ffeb1f9ad3b9c67d88e503
张雷 <zhanglei@yundait.com>
2020-08-26 17:05:10 +0800

    feat(全局): 集成thymeleaf
    集成thymeleaf，创建测试页面

--- src/main/java/tech/ufun/forum/controller/TestController.java
+++ src/main/java/tech/ufun/forum/controller/TestController.java
@@ -1,18 +1,18 @@
 package tech.ufun.forum.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * @author zhanglei
  * @date 2020-08-26 10:18
  */
-@RestController
+@Controller
 @RequestMapping("/")
 public class TestController {
 
     @RequestMapping("/")
     public String test() {
-        return "Hello";
+        return "test";
     }
 }
